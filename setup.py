49ffef9c69d2330c77e349801c0a4e3303eecacf
jensens <jensens@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-08-16 19:18:51 +0000

    dont need a header for setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-#
-# Copyright 2008, Blue Dynamics Alliance, Austria - http://bluedynamics.com
-#
-# GNU General Public Licence Version 2 or later
-
-__author__ = """Robert Niederreiter <rnix@squarewave.at>"""
-__docformat__ = 'plaintext'
-
 from setuptools import setup, find_packages
 import sys, os
 
